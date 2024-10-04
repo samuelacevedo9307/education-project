150b642b4f15de672b4979aa02061059551b44d0
samuelacevedo9307 <samuel.acevedo.dev@gmail.com>
2024-10-04 09:12:38 -0500

    feat: change in page avatar and dashboard , use client

--- src/app/pages/avatar/page.tsx
+++ src/app/pages/avatar/page.tsx
@@ -18,13 +18,15 @@ const Dashboard: React.FC = () => {
 
   return (
     <div className="flex flex-col w-full h-screen justify-center items-center">
-      <h1 className="p-5 text-4xl text-white">Bienvenido(a), {fullName}</h1>
+      <h1 className="p-5 text-4xl text-white">
+        Bienvenido(a), {fullName || "Invitado"}
+      </h1>
       <button className="btn btn-primary btn-block btn-large">
         <a href="/pages/dashboard" className="w-full">
           Siguiente
         </a>
       </button>
-      <div className=" h-screen w-full container-materials p-10">
+      <div className="h-screen w-full container-materials p-10">
         <iframe
           src="https://readyplayer.me/avatar?id=66ff29ccaacffe0b867f372d"
           className="w-full h-full"
