e02a380a8a45276c6f6cd278c8dee14673551354
samuelacevedo9307 <samuel.acevedo.dev@gmail.com>
2024-10-04 09:32:26 -0500

    feat: change in page avatar with fullname

--- src/app/pages/avatar/page.tsx
+++ src/app/pages/avatar/page.tsx
@@ -1,26 +1,9 @@
 "use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 const Dashboard: React.FC = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Verifica si el usuario no está registrado
-    const fullName = localStorage.getItem("fullName");
-    if (!fullName) {
-      router.push("/pages/register"); // Redirige a la página de registro si no hay nombre completo
-    }
-  }, [router]);
-
-  // Obtén el nombre completo del localStorage para mostrarlo
-  const fullName = localStorage.getItem("fullName");
-
   return (
     <div className="flex flex-col w-full h-screen justify-center items-center">
-      <h1 className="p-5 text-4xl text-white">
-        Bienvenido(a), {fullName || "Invitado"}
-      </h1>
+      <h1 className="p-5 text-4xl text-white">Bienvenido(a)</h1>
       <button className="btn btn-primary btn-block btn-large">
         <a href="/pages/dashboard" className="w-full">
           Siguiente
