4c581c2bb79b774337f65409aec6f2d2ec9bfedb
samuelacevedo9307 <samuel.acevedo.dev@gmail.com>
2024-10-04 08:34:53 -0500

    feat: change to avatar page function

--- src/app/pages/avatar/page.tsx
+++ src/app/pages/avatar/page.tsx
@@ -1,35 +1,37 @@
 "use client";
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const Dashboard: React.FC = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleLogout = () => {
+  useEffect(() => {
+    // Verifica si el usuario no está registrado
+    const fullName = localStorage.getItem("fullName");
+    if (!fullName) {
+      router.push("/pages/register"); // Redirige a la página de registro si no hay nombre completo
+    }
+  }, [router]);
 
-        router.push('/'); // Redirige al login
-    };
+  // Obtén el nombre completo del localStorage para mostrarlo
+  const fullName = localStorage.getItem("fullName");
 
-    useEffect(() => {
-        // Verifica si el usuario no está registrado
-        const fullName = localStorage.getItem('fullName');
-        if (!fullName) {
-            router.push('/pages/register'); // Redirige a la página de registro si no hay nombre completo
-        }
-    }, [router]);
-
-    // Obtén el nombre completo del localStorage para mostrarlo
-    const fullName = localStorage.getItem('fullName');
-
-    return (
-        <div className='flex flex-col w-full h-screen justify-center items-center'>
-            <h1 className='p-5 text-4xl text-white'>Bienvenido(a), {fullName}</h1>
-            <button className='btn btn-primary btn-block btn-large'><a href="/pages/dashboard" className='w-full'>Siguiente</a></button>
-            <div className=' h-screen w-full container-materials p-10'>
-                    <iframe src="https://readyplayer.me/avatar?id=66ff29ccaacffe0b867f372d" className='w-full h-full'></iframe>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col w-full h-screen justify-center items-center">
+      <h1 className="p-5 text-4xl text-white">Bienvenido(a), {fullName}</h1>
+      <button className="btn btn-primary btn-block btn-large">
+        <a href="/pages/dashboard" className="w-full">
+          Siguiente
+        </a>
+      </button>
+      <div className=" h-screen w-full container-materials p-10">
+        <iframe
+          src="https://readyplayer.me/avatar?id=66ff29ccaacffe0b867f372d"
+          className="w-full h-full"
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
