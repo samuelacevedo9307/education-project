405f6e58e2b7597abd86c4ae8268a6e20fad6f94
samuelacevedo9307 <samuel.acevedo.dev@gmail.com>
2024-10-04 09:23:26 -0500

    feat: change in page conect problems in localstorage

--- src/app/pages/conect/page.tsx
+++ src/app/pages/conect/page.tsx
@@ -1,31 +1,29 @@
 "use client";
 
+import Link from "next/link";
+
 const BrainQuiz: React.FC = () => {
-  const fullName = localStorage.getItem("fullName");
+  const fullName = localStorage.getItem("fullName") || "Usuario";
 
   return (
     <div className="h-screen w-full">
       <div className="w-full h-full flex flex-col justify-center items-center">
         <h1 className="p-10 text-4xl text-white">Hola, {fullName}</h1>
         <div className="w-full h-min flex justify-center items-center p-10">
-          <button
-            id="btn-before"
-            className="btn btn-primary btn-block btn-large mt-10 w-full "
+          <a
+            href="https://meet.google.com/pgy-bnbq-seg"
+            className="btn btn-primary btn-block btn-large mt-10 w-full"
           >
-            <a href="https://meet.google.com/pgy-bnbq-seg" className="w-full">
-              Ir a la clase
-            </a>
-          </button>
+            Ir a la clase
+          </a>
         </div>
         <div className="w-full h-min flex justify-center items-center p-10">
-          <button
-            id="btn-before"
-            className="btn btn-primary btn-block btn-large mt-10 w-full "
+          <Link
+            href="/dashboard"
+            className="btn btn-primary btn-block btn-large mt-10 w-full"
           >
-            <a href="/pages/dashboard" className="w-full">
-              Regresar
-            </a>
-          </button>
+            Regresar
+          </Link>
         </div>
       </div>
     </div>
