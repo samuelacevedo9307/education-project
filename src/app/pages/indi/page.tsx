a58207b31c5c25d1ebb0ab5aebd02f6804fc5fc6
samuelacevedo9307 <samuel.acevedo.dev@gmail.com>
2024-10-11 07:36:31 -0500

    feat:seleccion about IA and interaction avatar with information

--- src/app/pages/indi/page.tsx
+++ src/app/pages/indi/page.tsx
@@ -13,6 +13,9 @@ const Slider: React.FC = () => {
   // Estado para controlar la imagen actual
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  // Estado para controlar si la imagen ha sido seleccionada
+  const [selected, setSelected] = useState(false);
+
   // Función para ir a la imagen anterior
   const prevSlide = () => {
     const newIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
@@ -25,6 +28,11 @@ const Slider: React.FC = () => {
     setCurrentIndex(newIndex);
   };
 
+  // Función para seleccionar la imagen
+  const handleSelect = () => {
+    setSelected(true); // Cuando se selecciona, cambia el estado
+  };
+
   return (
     <div className="flex flex-col w-full h-5/5 justify-center items-center">
       <h1 className="p-5 text-4xl text-white">Escoge tu avatar</h1>
@@ -35,54 +43,86 @@ const Slider: React.FC = () => {
       </button>
 
       <div className="h-max w-full container-materials p-10 flex items-center justify-center">
-        {/* Botón para la imagen anterior */}
-        <button onClick={prevSlide}>
-          <img
-            src="/assets/icons/prev-arrow.png"
-            alt="flechaAtras"
-            className="w-12 h-12" // Tamaño pequeño de la flecha
-          />
-        </button>
+        {/* Botón para la imagen anterior*/}
+        {!selected && (
+          <button onClick={prevSlide}>
+            <img
+              src="/assets/icons/prev-arrow.png"
+              alt="flechaAtras"
+              className="w-12 h-12" // Tamaño pequeño de la flecha
+            />
+          </button>
+        )}
 
         {/* Imagen actual del slider */}
         <img
           src={images[currentIndex]}
           alt="icon"
-          className="w-h-96 h-96 cursor-pointer"
+          className={`w-h-96 h-96 cursor-pointer ${
+            selected ? "w-64 h-64" : ""
+          }`} // Aplica el tamaño más pequeño si está seleccionada
+          onClick={handleSelect}
         />
 
-        {/* Botón para la imagen siguiente */}
-        <button onClick={nextSlide}>
-          <img
-            src="/assets/icons/next-arrow.png"
-            alt="flechaDelante"
-            className="w-12 h-12" // Tamaño pequeño de la flecha
-          />
-        </button>
+        {/* Botón para la imagen siguiente  */}
+        {!selected && (
+          <button onClick={nextSlide}>
+            <img
+              src="/assets/icons/next-arrow.png"
+              alt="flechaDelante"
+              className="w-12 h-12" // Tamaño pequeño de la flecha
+            />
+          </button>
+        )}
       </div>
-      <div className="w-2/3 flex justify-center items-center">
-        <button className="btn btn-primary btn-block btn-large">
-          <a href="" className="w-full">
+
+      {/* Botón de seleccionar*/}
+      {!selected && (
+        <div className="w-2/3 flex justify-center items-center">
+          <button
+            className="btn btn-primary btn-block btn-large"
+            onClick={handleSelect}
+          >
             Seleccionar
-          </a>
-        </button>
-      </div>
-      <div className="contain-info-indi w-4/5 flex flex-col justify-center items-center p-10">
+          </button>
+        </div>
+      )}
+
+      <div className="contain-info-indi w-4/5 flex flex-col justify-center items-center p-20">
         <h2 className="text-white text-2xl font-bold mb-4">
           Los Guna Yala: Guardianes del Mar
         </h2>
+        <h2 className="text-white text-2xl font-bold mb-4">
+          Por que nuestros pueblos indígenas también merecen tener oportunidades
+        </h2>
+        <img
+          src="/assets/img/img-guna.jpeg"
+          alt="imgGuna"
+          className="p-10 h-2/4 w-2/4"
+        />
+        {/* Audio */}
+        <audio controls className="mt-4 mb-10">
+          <source
+            src="/assets/video/cancion-guna.mp3"
+            type="audio/mpeg"
+            className=""
+          />
+          Tu navegador no soporta la reproducción de audio.
+        </audio>
         <p className="text-white">
           Los Guna Yala, también conocidos como Kuna, son un pueblo indígena que
           habita en el archipiélago de San Blas, en Panamá. Famosos por sus
           coloridas molas, estas artesanías textiles son un símbolo de su rica
           cultura.
         </p>
+        <br />
         <p className="text-white">
           Los Guna Yala mantienen una estrecha relación con el mar, del cual
-          obtienen sus sustento a través de la pesca y la recolección de
+          obtienen su sustento a través de la pesca y la recolección de
           mariscos. Su cosmovisión se centra en el equilibrio entre la
           naturaleza y los seres humanos.
         </p>
+        <br />
         <ul className="list-disc text-white">
           <li>**Molas:** Un arte textil único y ancestral.</li>
           <li>**Comarcas:** Gobernanza autónoma en sus territorios.</li>
@@ -91,12 +131,13 @@ const Slider: React.FC = () => {
           </li>
         </ul>
       </div>
+
       <video
-        src="/assets/video/lenguage.mp4" // Reemplaza con la ruta de tu video
+        src="/assets/video/lenguage.mp4"
         autoPlay
         loop
         muted
-        className="fixed bottom-0 right-10 w-52 h-52 object-cover"
+        className="fixed bottom-1 right-5 w-52 h-52 object-cover"
         playsInline
       />
     </div>
--- src/app/pages/videoIA/page.tsx
+++ src/app/pages/videoIA/page.tsx
@@ -1,19 +1,55 @@
 "use client";
 
+import { useRef, useState } from "react";
 import Link from "next/link";
 
 const BrainQuiz: React.FC = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false); // Estado para saber si el video está reproduciéndose
+
+  const handlePlayPauseVideo = () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        videoRef.current.pause(); // Pausar el video
+        setIsPlaying(false); // Cambiar el estado a "no está reproduciéndose"
+      } else {
+        videoRef.current.muted = false; // Activar sonido
+        videoRef.current.play(); // Reproducir el video
+        setIsPlaying(true); // Cambiar el estado a "reproduciéndose"
+      }
+    }
+  };
+
   return (
     <div className="h-screen w-full">
-      <div className="w-full h-full flex flex-col justify-center items-center">
-        <h1 className="p-10 text-4xl text-white">Tareas </h1>
+      <div className="w-full flex flex-col justify-center items-center">
+        <h1 className="p-10 text-4xl text-white">Tareas</h1>
         <div>
-          <video width="600" height="600" controls>
-            <source src="/assets/video/videoia.mp4" type="video/mp4" />
-            Tu navegador no soporta la etiqueta de video.
-          </video>
-          <input type="file" className="w-fit bg-gray" />
+          {/* Video inicializa en mute */}
+          <video
+            ref={videoRef}
+            src="/assets/video/profe.mp4"
+            muted
+            loop
+            width="600"
+            height="600"
+            controls={false} // No mostrar controles del video
+          />
+          <div className="w-full h-min flex justify-center items-center p-5">
+            {/* Botón para alternar reproducción y pausa del video */}
+            <button
+              onClick={handlePlayPauseVideo}
+              className="btn btn-primary btn-block btn-large "
+            >
+              {isPlaying ? "Detener IA" : "Habla con la IA"}
+            </button>
+          </div>
+          <h3 className="text-white text-2xl text-center p-5">
+            Ingresa tu tarea aquí
+          </h3>
+          <input type="file" className="w-full bg-gray" />
         </div>
+
         <div className="w-full h-min flex justify-center items-center p-10">
           <Link
             href="/pages/dashboard"
