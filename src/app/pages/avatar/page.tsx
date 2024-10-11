345867a559348b0e04c484202ff0f5acad66bf66
samuelacevedo9307 <samuel.acevedo.dev@gmail.com>
2024-10-11 10:57:52 -0500

    feat: button in avatar page

--- src/app/pages/avatar/page.tsx
+++ src/app/pages/avatar/page.tsx
@@ -1,20 +1,21 @@
 "use client";
+import Link from "next/link";
 
 const Dashboard: React.FC = () => {
   return (
-    <div className="flex flex-col w-full h-max justify-center items-center">
+    <div className="flex flex-col w-full h-max justify-center items-center p-10">
       <h1 className="p-5 text-4xl text-white">Bienvenido(a)</h1>
-      <button className="btn btn-primary btn-block btn-large">
-        <a href="/pages/dashboard" className="w-full">
-          Siguiente
-        </a>
-      </button>
       <div className="h-screen w-full container-materials p-10">
         <iframe
           src="https://readyplayer.me/avatar?id=66ff29ccaacffe0b867f372d"
           className="w-full h-full"
         ></iframe>
       </div>
+      <Link href="/pages/dashboard" className="w-full flex justify-center items-center ">
+        <button className="btn btn-primary btn-block btn-large">
+          Siguiente
+        </button>
+      </Link>
     </div>
   );
 };
